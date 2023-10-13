a553a02c49942076f607bb3d01b0aa8cbec2009b
SoslanZ <zagegovski@yandex.ru>
2023-10-14 00:48:24 +0300

    aston_test_task

--- src/main/java/com/example/aston_task_jun/bankaccount/dao/AccountInfoRepository.java
+++ src/main/java/com/example/aston_task_jun/bankaccount/dao/AccountInfoRepository.java
@@ -3,9 +3,11 @@ package com.example.aston_task_jun.bankaccount.dao;
 import com.example.aston_task_jun.bankaccount.entity.AccountInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AccountInfoRepository extends JpaRepository<AccountInfo, String> {
 
     @Query(value = "SELECT account_number, name, balance FROM Account", nativeQuery = true)
